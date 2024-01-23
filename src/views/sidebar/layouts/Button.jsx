9eea095240f021f87bb4b58dbb4454804122bab9
Bahari hari <baharihari49@gmail.com>
2024-01-23 21:18:56 +0700

    fix button

--- src/views/sidebar/layouts/Button.jsx
+++ src/views/sidebar/layouts/Button.jsx
@@ -2,7 +2,7 @@
 const Button = ({event, bgColour, textColour, label, type}) => {
     return(
         <button style={{backgroundColor: bgColour, color: textColour}} onClick={event} type={type}
-        className={`w-full text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-[${{bgColour}}] font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}>
+        className={`w-full text-white hover:bg-primary-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}>
             {label}
         </button>
     )
