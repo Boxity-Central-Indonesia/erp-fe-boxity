05783da4a56b90c9b8faa391201e945731006728
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-03-07 01:17:16 +0700

    update: package json and vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      // Import your main CSS file here
+      scss: {
+        additionalData: `@import "src/styles/tailwind.css";`,
+      },
+    },
+  },
   plugins: [react()],
   build: {
     rollupOptions: {
